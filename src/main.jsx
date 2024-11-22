f0aaf5946a44ff4f3601804a3618c1f0515d88d4
PdaDmitry <pda.gazcomp@gmail.com>
2024-11-22 13:56:26 +0200

    configur redux, create reducer and  connect to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react';
+// import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
+  // </StrictMode>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { counterReducer } from './counterLogics/reducer';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
