1e7eaaacb39160ded329c5f64214b678b4bc13e9
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-11-23 14:40:07 +0200

    createReducer

--- src/redux/counterLogics/reducer.js
+++ src/redux/counterLogics/reducer.js
@@ -1,3 +1,4 @@
+import { createReducer } from '@reduxjs/toolkit';
 import { changeStep, decrement, increment, reset } from './actions';
 // import { CHANGE_STEP, DECREMENT, INCREMENT, RESET } from './constants';
 
@@ -6,34 +7,51 @@ const initialState = {
   stepNow: 1,
 };
 
-export const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    // case INCREMENT:  // <= when we use constants
-    case increment.type:
-      return {
-        ...state,
-        counterNow: state.counterNow + state.stepNow,
-      };
-    // case DECREMENT:
-    case decrement.type:
-      return {
-        ...state,
-        counterNow: state.counterNow - state.stepNow,
-      };
-    // case RESET:
-    case reset.type:
-      return {
-        ...state,
-        counterNow: 0,
-        stepNow: 1,
-      };
-    // case CHANGE_STEP:
-    case changeStep.type:
-      return {
-        ...state,
-        stepNow: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+export const counterReducer = createReducer(initialState, builder => {
+  builder
+    .addCase(increment, (state, action) => {
+      state.counterNow += state.stepNow;
+    })
+    .addCase(decrement, (state, action) => {
+      state.counterNow -= state.stepNow;
+    })
+    .addCase(reset, (state, action) => {
+      return initialState;
+    })
+    .addCase(changeStep, (state, action) => {
+      state.stepNow = action.payload;
+    });
+});
+
+//we can use this construction
+// export const counterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     // case INCREMENT:  // <= when we use constants
+//     case increment.type:
+//       return {
+//         ...state,
+//         counterNow: state.counterNow + state.stepNow,
+//       };
+//     // case DECREMENT:
+//     case decrement.type:
+//       return {
+//         ...state,
+//         counterNow: state.counterNow - state.stepNow,
+//       };
+//     // case RESET:
+//     case reset.type:
+//       return {
+//         ...state,
+//         counterNow: 0,
+//         stepNow: 1,
+//       };
+//     // case CHANGE_STEP:
+//     case changeStep.type:
+//       return {
+//         ...state,
+//         stepNow: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
